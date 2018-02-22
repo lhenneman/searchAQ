c18a2f9e4391bb829166749f786b07bf76c6efea
Lucas Henneman <lhenneman@gmail.com>
2018-02-22 07:45:06 -0500

    spacing

--- R/mean_which8h.R
+++ R/mean_which8h.R
@@ -1,11 +1,11 @@
 #get the mda8
 mean_which8h <- function( Z,
                           Z.which){
-  Z.8h <- rep( NA, ength( Z.which))
+  Z.8h <- rep( NA, length( Z.which))
   for ( d in 2:length( Z.which)){
     if ( is.na( Z.which[d]) == T) next
     h.which <- ( Z.which[d]):( Z.which[d] + 7)
-    Z.8h[d] <- mean( Z[h.which,d])
+    Z.8h[d] <- mean( Z[h.which, d])
   }
   return( Z.8h)
 }
